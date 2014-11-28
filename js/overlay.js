11b7d31b0b832cce90f502b5e3187b8e8fa2bd4c
adamxp12 <adamxp12@adamxp12.com>
2014-11-28 21:58:36 +0000

    Make some style changes to overlay

--- js/overlay.js
+++ js/overlay.js
@@ -1,3 +1,5 @@
+// Terrible Code (C) Adamxp12.com
+// Created for use on VictusBuilds.ga
 function showoverlay() {
   document.getElementById('overlay').style.visibility = 'visible'
   document.getElementById('overlay').style.opacity = '1'
