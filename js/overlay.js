9f4f8f667baab24d2a3eaad32684dc08acfaf4cd
adamxp12 <adamxp12@adamxp12.com>
2014-11-28 21:43:42 +0000

    Add overlay

---
+++ js/overlay.js
@@ -0,0 +1,9 @@
+function showoverlay() {
+  document.getElementById('overlay').style.visibility = 'visible'
+  document.getElementById('overlay').style.opacity = '1'
+};
+
+function hideoverlay() {
+  document.getElementById('overlay').style.opacity = '0'
+  document.getElementById('overlay').style.visibility = 'hidden'
+}
