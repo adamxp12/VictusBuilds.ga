19f03c761e95837a3e5fdce4ac180ae09f888d60
adamxp12 <adamxp12@adamxp12.com>
2015-01-05 19:12:37 +0000

    Update to new template

---
+++ old/js/overlay.js
@@ -0,0 +1,11 @@
+// Terrible Code (C) Adamxp12.com
+// Created for use on VictusBuilds.ga
+function showoverlay() {
+  document.getElementById('overlay').style.visibility = 'visible'
+  document.getElementById('overlay').style.opacity = '1'
+};
+
+function hideoverlay() {
+  document.getElementById('overlay').style.opacity = '0'
+  document.getElementById('overlay').style.visibility = 'hidden'
+}
